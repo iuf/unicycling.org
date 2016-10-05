6004f0c05fd5da5ce650011c679148a4207e706b
gossi <github@gos.si>
2016-10-05 14:46:45 +0200

    Initial package

---
+++ src/UnicyclingApplication.php
@@ -0,0 +1,21 @@
+<?php
+namespace iuf\unicycling\app;
+
+use keeko\framework\foundation\AbstractApplication;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Iuf Unicycling.org
+ * 
+ * @license MIT
+ * @author gossi
+ */
+class UnicyclingApplication extends AbstractApplication {
+
+	/**
+	 * @param Request $request
+	 * @param string $path
+	 */
+	public function run(Request $request, $path) {
+	}
+}
